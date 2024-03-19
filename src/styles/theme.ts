dd7259a31a1595182df149aa8a500945cbb2cc8a
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2024-03-19 17:03:49 -0300

    Add: Styled-Components and typescript configs

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    red: '#F37272',
+    blue950: '#1C1B1F',
+    title: '#1F2024',
+    primary: '#006FFD',
+    text: '#5D6570',
+    border: '#5D6570',
+    textMuted: '#71727A',
+    background: '#F6F8FC',
+    grey50: '#F7F9FC',
+  },
+}
