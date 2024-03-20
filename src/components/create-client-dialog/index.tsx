253425a0d60a9d5fb6cde954130158ad8fda5455
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2024-03-20 16:25:25 -0300

    Add: form validation in create client form

--- src/components/create-client-dialog/index.tsx
+++ src/components/create-client-dialog/index.tsx
@@ -11,46 +11,135 @@ import { IconButton } from '../icon-button'
 import { XIcon } from '../icons/x-icon'
 import { CreateClientDialogBody } from './styles'
 import { Input } from '../input'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
 
-export const CreateClientDialog: React.FC = () => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <Button>+ Novo Cliente</Button>
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <DialogOverlay>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Cadastrar Cliente</DialogTitle>
-            <Dialog.Close asChild>
-              <IconButton>
-                <XIcon />
-              </IconButton>
-            </Dialog.Close>
-          </DialogHeader>
-          <CreateClientDialogBody>
-            <form>
-              <Input label='Nome' />
-              <Input label='CNPJ' />
-              <Input label='Telefone' />
-              <Input label='CEP' />
-              <Input label='Estado' />
-              <Input label='Cidade' />
-              <Input label='Bairro' />
-              <Input label='Endereço' />
-              <Input label='Número' />
-            </form>
-          </CreateClientDialogBody>
-          <DialogFooter>
-            <Button
-              size='small'
-              style={{ marginLeft: 'auto' }}
-            >
-              Salvar
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </DialogOverlay>
-    </Dialog.Portal>
-  </Dialog.Root>
-)
+const schema = yup.object({
+  name: yup.string().required('Este campo é obrigatório.'),
+  CNPJ: yup.string().required('Este campo é obrigatório.'),
+  phone: yup.string().required('Este campo é obrigatório.'),
+  CEP: yup.string().required('Este campo é obrigatório.'),
+  state: yup.string().required('Este campo é obrigatório.'),
+  city: yup.string().required('Este campo é obrigatório.'),
+  neighborhood: yup.string().required('Este campo é obrigatório.'),
+  address: yup.string().required('Este campo é obrigatório.'),
+  number: yup
+    .number()
+    .positive('Apenas números maiores que 0.')
+    .required('Este campo é obrigatório.'),
+})
+
+export interface FormInput {
+  name: string
+  CNPJ: string
+  phone: string
+  CEP: string
+  state: string
+  city: string
+  neighborhood: string
+  address: string
+  number: number
+}
+
+export const CreateClientDialog: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormInput>({
+    resolver: yupResolver(schema),
+  })
+
+  const onSubmit = (data: FormInput) => {
+    console.log(data)
+    reset()
+  }
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <Button>+ Novo Cliente</Button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <DialogOverlay>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Cadastrar Cliente</DialogTitle>
+              <Dialog.Close asChild>
+                <IconButton>
+                  <XIcon />
+                </IconButton>
+              </Dialog.Close>
+            </DialogHeader>
+            <CreateClientDialogBody>
+              <form
+                id='create-client'
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <Input
+                  label='Nome'
+                  {...register('name')}
+                  helperText={errors.name?.message}
+                />
+                <Input
+                  label='CNPJ'
+                  {...register('CNPJ')}
+                  helperText={errors.CNPJ?.message}
+                />
+                <Input
+                  label='Telefone'
+                  {...register('phone')}
+                  helperText={errors.phone?.message}
+                />
+                <Input
+                  label='CEP'
+                  {...register('CEP')}
+                  helperText={errors.CEP?.message}
+                />
+                <Input
+                  label='Estado'
+                  {...register('state')}
+                  helperText={errors.state?.message}
+                />
+                <Input
+                  label='Cidade'
+                  {...register('city')}
+                  helperText={errors.city?.message}
+                />
+                <Input
+                  label='Bairro'
+                  {...register('neighborhood')}
+                  helperText={errors.neighborhood?.message}
+                />
+                <Input
+                  label='Endereço'
+                  {...register('address')}
+                  helperText={errors.address?.message}
+                />
+                <Input
+                  label='Número'
+                  {...register('number')}
+                  type='number'
+                  helperText={errors.number?.message}
+                />
+              </form>
+            </CreateClientDialogBody>
+            <DialogFooter>
+              <Button
+                size='small'
+                type='submit'
+                form='create-client'
+                formNoValidate
+                style={{ marginLeft: 'auto' }}
+              >
+                Salvar
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </DialogOverlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,23 +1,23 @@
+import { forwardRef } from 'react'
 import { InputLabel, InputWrapper } from './styles'
 import { InputProps } from './types'
 
-export const Input: React.FC<InputProps> = ({
-  suffix,
-  helperText,
-  label,
-  ...otherProps
-}) => {
-  return (
-    <InputLabel>
-      <p className='label'>{label}</p>
-      <InputWrapper>
-        <input
-          type='text'
-          {...otherProps}
-        />
-        {suffix}
-      </InputWrapper>
-      {helperText && <p className='helper-text'>{helperText}</p>}
-    </InputLabel>
-  )
-}
+export const Input: React.FC<InputProps> = forwardRef<
+  HTMLInputElement,
+  InputProps
+>(({ suffix, helperText, label, ...otherProps }, ref) => (
+  <InputLabel>
+    <p className='label'>{label}</p>
+    <InputWrapper>
+      <input
+        type='text'
+        ref={ref}
+        {...otherProps}
+      />
+      {suffix}
+    </InputWrapper>
+    {helperText && <p className='helper-text'>{helperText}</p>}
+  </InputLabel>
+))
+
+Input.displayName = 'Input'
