a88e0a28b73bdfc219c0516f5b79063390236fc1
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2024-03-20 18:23:57 -0300

    Add: Search function in client page

--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -9,9 +9,21 @@ import { ClientsList, Wrapper } from './styles'
 
 import { useSelector } from 'react-redux'
 import { RootState } from '../../states/store'
+import { useState } from 'react'
+import { IClient } from '../../@types/client'
 
 export const Clients: React.FC = () => {
+  const [filter, setFilter] = useState<string>('')
   const clients = useSelector((state: RootState) => state.clients.clients)
+  const filteredClients = clients.filter((client) =>
+    Object.keys(client).some(
+      (key) =>
+        client[key as keyof IClient]
+          .toString()
+          .toLowerCase()
+          .indexOf(filter) !== -1
+    )
+  )
 
   return (
     <main>
@@ -19,9 +31,11 @@ export const Clients: React.FC = () => {
 
       <Container>
         <Wrapper>
-          <form action=''>
+          <form>
             <Input
               placeholder='Pesquisar'
+              onChange={(event) => setFilter(event.target.value)}
+              value={filter}
               suffix={
                 <IconButton aria-label='Pesquisar'>
                   <SearchIcon />
@@ -33,7 +47,7 @@ export const Clients: React.FC = () => {
         </Wrapper>
 
         <ClientsList>
-          {clients?.map((client) => (
+          {filteredClients?.map((client) => (
             <li key={client.CNPJ}>
               <ClientCard
                 name={client.name}
