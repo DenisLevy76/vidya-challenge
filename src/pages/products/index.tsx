f61812b2e9b3c46a7f5ef7dd2ce22e73c60c5387
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2024-03-21 12:43:52 -0300

    Add: Products page and product card

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,43 @@
+import { CreateOrderDialog } from '../../components/create-order-dialog'
+import { Header } from '../../components/header'
+import { IconButton } from '../../components/icon-button'
+import { SearchIcon } from '../../components/icons/search'
+import { Input } from '../../components/input'
+import { ProductCard } from '../../components/product-card'
+import { Container } from '../../styles/container'
+import { ProductList, Wrapper } from './styles'
+
+export const Products: React.FC = () => {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Container>
+          <Wrapper>
+            <Input
+              placeholder='Pesquisar'
+              suffix={
+                <IconButton aria-label='Pesquisar'>
+                  <SearchIcon />
+                </IconButton>
+              }
+            />
+            <CreateOrderDialog />
+          </Wrapper>
+          <ProductList>
+            <li>
+              <ProductCard />
+            </li>
+            <li>
+              <ProductCard />
+            </li>
+            <li>
+              <ProductCard />
+            </li>
+          </ProductList>
+        </Container>
+      </main>
+    </>
+  )
+}
--- src/states/clientSlice/index.tsx
+++ src/states/clientSlice/index.tsx
@@ -2,52 +2,7 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 import { IClient } from '../../@types/client'
 
 const initialState: { clients: IClient[] } = {
-  clients: [
-    {
-      name: 'Apple Inc.',
-      CNPJ: '12.345.678/9012-34',
-      phone: '(12) 3456-7890',
-      CEP: '12345-678',
-      state: 'California',
-      city: 'Cupertino',
-      neighborhood: 'Silicon Valley',
-      address: '1 Infinite Loop',
-      number: 123,
-    },
-    {
-      name: 'Google LLC',
-      CNPJ: '56.789.012/3456-78',
-      phone: '(98) 7654-3210',
-      CEP: '54321-876',
-      state: 'California',
-      city: 'Mountain View',
-      neighborhood: 'Bay Area',
-      address: '1600 Amphitheatre Parkway',
-      number: 456,
-    },
-    {
-      name: 'Amazon.com Inc.',
-      CNPJ: '90.123.456/7890-12',
-      phone: '(13) 5792-4680',
-      CEP: '98765-432',
-      state: 'Washington',
-      city: 'Seattle',
-      neighborhood: 'Downtown',
-      address: '410 Terry Avenue North',
-      number: 789,
-    },
-    {
-      name: 'Sony Corporation',
-      CNPJ: '34.567.890/1234-56',
-      phone: '(24) 6801-3579',
-      CEP: '87654-321',
-      state: 'Tokyo',
-      city: 'Tokyo',
-      neighborhood: 'Ginza',
-      address: '1-7-1 Konan',
-      number: 987,
-    },
-  ],
+  clients: [],
 }
 
 export const clientsSlice = createSlice({
