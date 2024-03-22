3a63503159e881ab529396e619ca70efae1fd3f6
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2024-03-21 21:23:37 -0300

    Add: GlobalStyles component

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './styles/theme.ts'
 
-import './styles/reset.css'
 import { Provider } from 'react-redux'
 import { store } from './states/store.ts'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './libs/react-query.tsx'
 import { App } from './App.tsx'
+import { GlobalStyles } from './styles/reset.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -16,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <App />
+          <GlobalStyles />
         </ThemeProvider>
       </Provider>
     </QueryClientProvider>
