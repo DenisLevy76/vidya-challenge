b69114f03d24bd8113f0b31cc132da55b978775e
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2024-03-22 18:31:09 -0300

    Update: removed unused libraries

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,16 @@ import { theme } from './styles/theme.ts'
 
 import { Provider } from 'react-redux'
 import { store } from './states/store.ts'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './libs/react-query.tsx'
 import { App } from './App.tsx'
 import { GlobalStyles } from './styles/reset.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <App />
-          <GlobalStyles />
-        </ThemeProvider>
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+        <GlobalStyles />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
