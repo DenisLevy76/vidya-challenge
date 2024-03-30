b0a74813be75c89f4ac8ca3fa391b645d758c943
DenisLevy76 <denis.levy11@gmail.com>
2024-03-29 23:38:59 -0300

    Update: removed all console.log's from the code

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,19 @@ export const App: React.FC = () => {
   const clients = useSelector((state: RootState) => state.clients.clients)
   const orders = useSelector((state: RootState) => state.orders.orders)
   const products = useSelector((state: RootState) => state.products.products)
+
   const [loading, setLoading] = useState<boolean>(true)
+
   const dispatch = useDispatch()
 
   useEffect(() => {
     if (!loading) return
-
-    console.log('teste1')
 
     const persistentOrdersState = localStorage.getItem('orders')
     const persistentClientState = localStorage.getItem('clients')
     const persistentProductState = localStorage.getItem('products')
 
     if (persistentProductState && persistentProductState?.length > 2) {
-      console.log(persistentProductState)
       const data = JSON.parse(persistentProductState)
       dispatch(initializeProductState(data))
     }
@@ -35,24 +34,19 @@ export const App: React.FC = () => {
     }
 
     if (persistentOrdersState && persistentOrdersState?.length > 2) {
-      console.log(persistentOrdersState)
       const data = JSON.parse(persistentOrdersState)
       dispatch(initializeOrdersState(data))
     }
-
-    console.log('teste2')
 
     setLoading(false)
   }, [dispatch, loading])
 
   useEffect(() => {
     if (loading) return
-    console.log('teste3')
 
     localStorage.setItem('clients', JSON.stringify(clients))
     localStorage.setItem('products', JSON.stringify(products))
     localStorage.setItem('orders', JSON.stringify(orders))
-    console.log('teste4')
   }, [clients, products, loading, orders])
 
   return <RouterProvider router={routes} />
